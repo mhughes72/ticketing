f60e84110e741f2e1d6185dc9743be08034beae6
Matt Hughes <matthughes72@gmail.com>
2021-04-28 19:27:09 +0100

    fuck you gith

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
-  console.log('starting up!');
+  console.log('starting up 2!');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
